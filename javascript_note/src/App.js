a067593c341f0d89fb598bd566f39a2ca965aaeb
Julio Pampuch <juliopampuch@gmail.com>
2023-09-25 10:34:38 -0300

    created home screen

--- javascript_note/src/App.js
+++ javascript_note/src/App.js
@@ -1,10 +1,11 @@
 import './App.scss';
 import { Notification, Section } from 'rbx'
 import Header from './components/header';
+import HomeScreen from './screens/home';
 
 function App() {
   return (
-    <Header />
+    <HomeScreen />
   );
 }
 
