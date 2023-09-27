3d528d82b8d8f5d0e710ce6541c78d1af4b52a0f
Julio Pampuch <juliopampuch@gmail.com>
2023-09-26 23:21:10 -0300

    creating register page

--- javascript_note/src/screens/auth/register/index.js
+++ javascript_note/src/screens/auth/register/index.js
@@ -1,7 +1,32 @@
+import { Card, Column, Container, Section, Title } from "rbx"
+import Header from "../../../components/header"
+import LogoImage from '../../../assets/images/logo.png'
+import '../../../styles/auth.scss'
+
 const RegisterScreen = () => {
   return (
     <>
-    test
+      <Header />
+      <Section size="medium" className="auth">
+        <Container>
+          <Column.Group centered vcentered>
+            <Column size={3}>
+              <Card>
+                <Card.Content>
+                  <Column size={12}>
+                      <img src={LogoImage} />
+                  </Column>
+                  <Column size={12}>
+                    <Title size={6} className="has-text-grey has-text-centered">
+                      Your notes on the cloud
+                    </Title>
+                  </Column>
+                </Card.Content>
+              </Card>
+            </Column>
+          </Column.Group>
+        </Container>
+      </Section>
     </>
   )
 }
